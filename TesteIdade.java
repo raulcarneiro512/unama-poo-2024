8358806569077ddf282e12733b30b655c1208fc0
Raul Carneiro <raulgabriel.oliveira@gmail.com>
2024-08-22 00:52:38 +0000

    add:

---
+++ TesteIdade.java
@@ -0,0 +1,9 @@
+ class TesteIdade {
+        public static void main(String[] args) {
+            
+            int idade;
+            idade = 28;
+
+            System.out.println(idade);
+        }
+}
